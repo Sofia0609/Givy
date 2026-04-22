d9a205d336da852831824ed4634f65dc5c3a80d7
Sofia Velez <sofiavelezcotillo@gmail.com>
2026-04-22 18:01:53 -0500

    feat: Add react router

--- Givy Project/src/main.tsx
+++ Givy Project/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Feed from './pages/feed/Feed'
+import {  createBrowserRouter, RouterProvider } from 'react-router'
+
+
+let routes = createBrowserRouter([{
+  path: "/",
+  element: <Feed></Feed>
+}],)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Feed></Feed>
+    <RouterProvider router={routes}></RouterProvider>
   </StrictMode>,
 )
