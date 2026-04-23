f902a765335dd4577f2039932c1cf3310a33b64c
Sofia Velez <sofiavelezcotillo@gmail.com>
2026-04-22 23:05:28 -0500

    style: Add color variables

--- Givy Project/src/pages/notificacions/Notifications.tsx
+++ Givy Project/src/pages/notificacions/Notifications.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import NavBar from '../../components/navBar/navBar'
+import Banner from '../../components/notifications/banner/banner'
 
 
 function Notifications() {
 
   return (
-    <>
+    <><div className='notificationsLayout'>
       <NavBar></NavBar>
+      <Banner title="In this sections you can see your Interactions Posibles Swaps, and Matches!"></Banner>
+    </div>
     </>
   )
 }
