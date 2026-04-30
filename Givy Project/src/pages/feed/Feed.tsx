5554abd6f3657b0886250fcf56daa97d00f80f22
Paula Camayo <paca@MacBook-Air-de-Paula.local>
2026-04-30 10:32:09 -0500

    Feed: Components modification

--- Givy Project/src/pages/feed/Feed.tsx
+++ Givy Project/src/pages/feed/Feed.tsx
@@ -3,12 +3,9 @@ import React, { useState } from "react";
 // Importar subcomponentes del feed
 import Description from "../../components/feed/description/description";
 import VideoSection from "../../components/feed/videoSection/VideoSection";
-import SidebarRight from "../../components/feed/sidebarRight/SidebarRight";
 import CircularButton from "../../components/feed/circularButton/CircularButton";
 import Comments from "../../components/feed/comments/comments";
 import ProfileButton from "../../components/feed/profileButton/ProfileButton";
-import SwapModal from "../../components/feed/swap/SwapModal";
-
 // Importar la data
 import users from "../../data/users.json";
 import videos from "../../data/videos.json";
@@ -16,9 +13,8 @@ import videos from "../../data/videos.json";
 const Feed: React.FC = () => {
   const [liked, setLiked] = useState(false);
   const [likeCount, setLikeCount] = useState(0);
-  const [showComments, setShowComments] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-
+  const [showComments] = useState(false);
+  
   return (
     <div className="feed">
       {users.map((user, index) => (
@@ -30,10 +26,7 @@ const Feed: React.FC = () => {
           <VideoSection video={videos[index].url} />
 
           {/* Barra lateral derecha con botones */}
-          <SidebarRight
-            onToggleComments={() => setShowComments(!showComments)}
-            onToggleModal={() => setShowModal(true)}
-          >
+          
             {/* Botón de perfil */}
             <ProfileButton />
 
@@ -47,13 +40,13 @@ const Feed: React.FC = () => {
                 setLiked(!liked);
               }}
             />
-          </SidebarRight>
+         
 
           {/* Sección de comentarios */}
           {showComments && <Comments />}
 
           {/* Modal de swap */}
-          <SwapModal show={showModal} onClose={() => setShowModal(false)} />
+          
         </div>
       ))}
     </div>
