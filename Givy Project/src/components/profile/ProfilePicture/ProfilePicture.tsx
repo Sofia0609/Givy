c00757918ae79ea1948661052c7ecb0474ed2c86
Maria Juliana Henao <mjhenaob@gmail.com>
2026-05-01 08:13:17 -0500

    Components profile edits: ProfileName, ProfilePicture and ProfileName

--- Givy Project/src/components/profile/ProfilePicture/ProfilePicture.tsx
+++ Givy Project/src/components/profile/ProfilePicture/ProfilePicture.tsx
@@ -1,14 +1,21 @@
 import './ProfilePicture.css'
+
 interface ProfilePicture {
-    src: string
+    src?: string
     alt?: string
-    size?: 'smallder' | 'medium' | 'large'
+    size?: 'small' | 'medium' | 'large'
 }
-function ProfilePicture({ src, alt='Profile Picture', size='large' }: ProfilePicture) {
+
+function ProfilePicture({ src, alt = 'Profile Picture', size = 'large' }: ProfilePicture) {
     return (
-        <div className={`profilePictureWrapper  profilePictureWrapper--${size}`}>
-            <img className="profilePicture" src={src} alt={alt} />
+        <div className={`profilePictureWrapper profilePictureWrapper--${size}`}>
+            <img 
+                className="profilePicture" 
+                src={src || "https://www.gravatar.com/avatar/?d=mp&s=150"} 
+                alt={alt} 
+            />
         </div>
     )
 }
+
 export default ProfilePicture
--- Givy Project/src/pages/profile/Profile.tsx
+++ Givy Project/src/pages/profile/Profile.tsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
 import NavBar from '../../components/navBar/navBar'
-
+import ProfilePicture from '../../components/profile/ProfilePicture/ProfilePicture'
+import ProfileName from '../../components/profile/ProfileName/ProfileName'
+import UserInfo from '../../components/profile/UserInfo/UserInfo'
+import './Profile.css'
 
 function Profile() {
-
   return (
-    <>
-      <NavBar></NavBar>
-    </>
+    <div className="profileLayout">
+      <NavBar />
+
+      <main className="profileMain">
+        <ProfilePicture src="https://placehold.co/150" size="large" />
+        <ProfileName name="Your Name" username="yourname0" />
+        <div className="profileStats">
+          <UserInfo label="Following" count={30} />
+          <UserInfo label="Followers" count={126} />
+          <UserInfo label="Videos" count={3} />
+          <UserInfo label="Reputation" count={8.6} />
+        </div>
+      </main>
+
+    </div>
   )
 }
 
