49b4ef438ddeeee608ee19b359433fb0e1e43100
Maria Juliana Henao <mjhenaob@gmail.com>
2026-05-01 10:09:24 -0500

    Editions in Profile.css

--- Givy Project/src/components/profile/VideoCard/VideoCard.tsx
+++ Givy Project/src/components/profile/VideoCard/VideoCard.tsx
@@ -1,12 +1,11 @@
 interface VideoCardProps {
     title?: string
     author?: string
-    description?: string
     thumbnail?: string
     views?: number
 }
 
-function VideoCard({ title, author, description, thumbnail, views }: VideoCardProps) {
+function VideoCard({ title, author, thumbnail, views }: VideoCardProps) {
     return (
         <div className="videoCard">
             <div className="videoCardThumbnail">
@@ -18,7 +17,6 @@ function VideoCard({ title, author, description, thumbnail, views }: VideoCardPr
             <div className="videoCardInfo">
                 {title && <span className="videoCardTitle">{title}</span>}
                 {author && <span className="videoCardAuthor">{author}</span>}
-                {description && <p className="videoCardDescription">{description}</p>}
                 {views !== undefined && <span className="videoCardViews">{views} views</span>}
             </div>
         </div>
