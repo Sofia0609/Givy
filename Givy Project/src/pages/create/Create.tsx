55865202da2fdf02badbe5268ed21a944c6675c9
Sofia Velez <sofiavelezcotillo@gmail.com>
2026-05-07 12:22:11 -0500

    feat: create a new video as an object

--- Givy Project/src/pages/create/Create.tsx
+++ Givy Project/src/pages/create/Create.tsx
@@ -4,8 +4,48 @@ import Header from '../../components/header/header'
 import BigButton from '../../components/bigButton/bigButton'
 import UploadVideo from '../../components/create/uploadVideo/uploadVideo'
 import InputGivy from '../../components/inputGivy/inputGivy'
+import users from "../../data/users.json"
+import videos from "../../data/videos.json"
+import tagsData from '../../data/tags.json'
+import { useState } from 'react'
+
+const userLogged = "u1"
 
 function Create() {
+
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
+  const [videoUrl, setVideoUrl] = useState<string | null>(null)
+  const [description, setDescription] = useState('')
+  const [tagToTeach, setTagToTeach] = useState('')
+  const [tagToLearn, setTagToLearn] = useState('')
+
+  function handleVideoSelect(file: File) {
+    setSelectedFile(file)
+    const url = URL.createObjectURL(file)
+    setVideoUrl(url)
+  }
+
+        const stored = localStorage.getItem('videos')
+        const allVideos = stored ? JSON.parse(stored) : videos
+
+  function pusblishVideo(){
+
+    const newVideo ={
+
+            id: `v${allVideos.length + 1}`,
+            userId: userLogged,
+            matchId: null,
+            url: videoUrl,
+            thumbnail: videoUrl,
+            title: description,
+            description: description,
+            tags: [tagToTeach],
+            likes: 0,
+            uploadDate: new Date().toISOString(),
+            teaches: [tagToTeach],
+            wantsToLearnInReturn: [tagToLearn]
+    }
+  }
 
   return (
     <><div className='createLayout'>
@@ -15,17 +55,20 @@ function Create() {
       <div className='createContent'>
           <Header title="Upload your own video!"></Header>
           <UploadVideo
-            icon='./src/assets/uploaded_icon.svg'
+            icon='./src/assets/upload_icon.svg'
           />
           <BigButton content='Upload'></BigButton>
 
-          <InputGivy 
-            label="Title"
+          <InputGivy
+
+            label="Description"
             type="text"
-            value='Hola'
-            placeholder="placeholder"
+            value={description}
+            placeholder="Write something..."
+            onChange={e => setDescription(e.target.value)}  
             big={true}
           />
+
       </div>
     </div>
     </>
