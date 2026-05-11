8640f7f8087d1361eb9e0f5affbf94d3936d6d84
Sofia Velez <sofiavelezcotillo@gmail.com>
2026-05-11 12:39:32 -0500

    feat: Show the video when both user sent them

--- Givy Project/src/components/notifications/uploadVideoMatch/uploadVideoMatch.tsx
+++ Givy Project/src/components/notifications/uploadVideoMatch/uploadVideoMatch.tsx
@@ -2,23 +2,26 @@ import BigButton from '../../bigButton/bigButton'
 import UploadVideo from '../../create/uploadVideo/uploadVideo'
 import './UploadVideoMatch.css'
 
-interface UploadVideoMatchprops{
+interface UploadVideoMatchprops {
     tittle: string
     description: string
     icon: string
+    onVideoSelect?: (file: File) => void
+    disabled?: boolean
 }
 
-function UploadVideoMatch({tittle, description, icon}:UploadVideoMatchprops){
+function UploadVideoMatch({ tittle, description, icon, onVideoSelect, disabled }: UploadVideoMatchprops) {
 
-    return(
-        <>  
+    return (
         <div className='uploadVideoMatchContainer'>
-                <h2 className='tittleMatch'>  {tittle} </h2>
-                <UploadVideo icon={icon} />
-                <BigButton content='UPLOAD'/>
-                <p className='descriptionMatch'>  {description} </p>
-         </div>
-        </>
+            <h2 className='tittleMatch'>{tittle}</h2>
+            <UploadVideo
+                icon={icon}
+                onVideoSelect={disabled ? undefined : onVideoSelect}
+            />
+            <BigButton content='UPLOAD' />
+            <p className='descriptionMatch'>{description}</p>
+        </div>
     )
 }
 
