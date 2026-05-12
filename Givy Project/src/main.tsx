1773c1cc1c9c49986b7c294162a41b4f5db64646
Sofia Velez <sofiavelezcotillo@gmail.com>
2026-05-12 00:58:49 -0500

    feat: Add how Computer and Mobile match sections show

--- Givy Project/src/main.tsx
+++ Givy Project/src/main.tsx
@@ -56,6 +56,10 @@ let routes = createBrowserRouter([
     element: <Match />
   },
   {
+    path: "/match/:matchId",
+    element: <Match />
+  },
+  {
     path: '/Interactions',
     element: <Interactions />
   },
