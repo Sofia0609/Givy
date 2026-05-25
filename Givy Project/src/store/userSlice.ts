d7f5211282602ca6c9520636e60245a6a66a2d4f
Sofia Velez <sofiavelezcotillo@gmail.com>
2026-05-24 23:02:16 -0500

    feat: create user slice

---
+++ Givy Project/src/store/userSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+import type { User } from '../types/index' // ajusta el nombre
+
+interface UserState {
+  currentUser: User | null
+}
+
+const initialState: UserState = {
+  currentUser: null
+}
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.currentUser = action.payload
+    },
+    clearUser: (state) => {
+      state.currentUser = null
+    }
+  }
+})
+
+export const { setUser, clearUser } = userSlice.actions
+export default userSlice.reducer
