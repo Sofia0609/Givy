ef266507e8d8f4856039493b5be21ad23e1ae808
Paula Camayo <paulacamayo4@gmail.com>
2026-06-03 19:08:40 -0500

    feat: create store

---
+++ Givy Project/src/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from './userSlice'
+import videoReducer from './videoSlice'
+
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+    videos: videoReducer,
+  }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
