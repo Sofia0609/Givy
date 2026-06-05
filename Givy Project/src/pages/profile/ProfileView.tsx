5a13610e0f2f8517c1369fed4e04a72a6dc4f8a7
Maria Juliana Henao <mjhenaob@gmail.com>
2026-06-04 22:15:30 -0500

    Chore: remove mock data imports and add LoadingScreen/ErrorScreen to ProfileView

--- Givy Project/src/pages/profile/ProfileView.tsx
+++ Givy Project/src/pages/profile/ProfileView.tsx
@@ -10,6 +10,8 @@ import tags from '../../data/tags.json'
 import './ProfileStyle.css'
 import { useAppDispatch, useAppSelector } from '../../store/hooks'
 import { fetchProfileThunk, fetchProfileVideosThunk, followThunk, unfollowThunk, clearProfile } from '../../store/slices/profileSlice'
+import LoadingScreen from '../../components/guards/LoadingScreen'
+import ErrorScreen from '../../components/guards/ErrorScreen'
 
 const getTagNames = (tagIds: string[]) =>
   tagIds.map(id => tags.find(t => t.id === id)?.name || id)
@@ -38,8 +40,8 @@ function ProfileView() {
     }
   }, [userId, dispatch])
 
-  if (loading) return <div>Cargando perfil...</div>
-  if (error) return <div>Error: {error}</div>
+  if (loading) return <LoadingScreen />
+  if (error) return <ErrorScreen message={error} />
   if (!viewedUser) return (
     <div className="profileLayout">
       <NavBar />
